a4c4abaa03bcbffbd74d58bbc0424426d56ab45e
Jared Shanley <jaredshanley@gmail.com>
2026-09-09 14:13:58 +0100

    Changed the name of the function.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 FONT = ("Arial", 12, "bold")
 
 
-def button_clicked():
+def miles_to_kms():
     miles = float(input_miles.get())
     kms = miles * 1.60934
     input_kms.delete(0, tk.END)
@@ -11,15 +11,16 @@ def button_clicked():
 
 
 window = tk.Tk()
-window.title("Mile to Km Converter")
+window.title("Mile to Kilometer Converter")
+window.config(padx=20, pady=20)
 window.minsize(500, 300)
 
 # Entry for miles
-input_miles = tk.Entry(width=15)
+input_miles = tk.Entry(width=7)
 input_miles.grid(row=0, column=1)
 
 # Entry for kilometres
-input_kms = tk.Entry(width=15)
+input_kms = tk.Entry(width=7)
 input_kms.grid(row=1, column=1)
 
 miles = tk.Label(window, text="Miles", font=FONT)
@@ -32,7 +33,7 @@ km = tk.Label(window, text="Km", font=FONT)
 km.grid(row=1, column=2)
 
 # Creating a button
-button = tk.Button(text="Calculate", command=button_clicked)
+button = tk.Button(text="Calculate", command=miles_to_kms)
 button.grid(row=2, column=1)
 
 window.mainloop()
